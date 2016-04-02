f68485727d66eed76ba55a6fe7a3c5fa47886020
Seonho Kim <ksseono@gmail.com>
2016-04-02 22:33:36 +0900

    rearrange directories and project.clj / update CLP(Tree) content

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
 (defproject logic-tutorials-kr "1.0.0"
   :description "클로저 core.logic 튜토리얼"
-  
+  :url "http://logic-tutorials-kr.readthedocs.org"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}  
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [match "0.2.0-SNAPSHOT"]
                  [analyze "0.3.0"]
                  [org.clojure/tools.macro "0.1.5"]
                  [org.clojure/core.logic "0.8.10"]
-                 [org.clojure/data.json "0.2.6"]]
-  
-  :source-paths ["src/main/clojure"])
+                 [org.clojure/data.json "0.2.6"]])
